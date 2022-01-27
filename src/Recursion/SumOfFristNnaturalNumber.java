0235d716d5c4e6beb207d348c02691604079a838
deepzsenu <deepzsenu@gmail.com>
2022-01-27 20:38:50 +0530

    recursion

---
+++ src/Recursion/SumOfFristNnaturalNumber.java
@@ -0,0 +1,17 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class SumOfFristNnaturalNumber {
+    public static int sum(int n){
+        if(n==0){
+            return 0;
+        }
+        return n+sum(n-1);
+    }
+    public static void main(String[] args) {
+        Scanner sc  = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(sum(n));
+    }
+}
